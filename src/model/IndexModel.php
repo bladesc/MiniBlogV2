b092d56e5a8d771b430bcd38a5426f2aa376221d
Damian Szczesny <bladesc@interia.pl>
2019-12-01 00:32:49 +0100

    fixed IndeModel getData method

--- src/model/IndexModel.php
+++ src/model/IndexModel.php
@@ -6,7 +6,7 @@ namespace src\model;
 
 class IndexModel extends CommonModel
 {
-    public function getData()
+    public function getData(): array
     {
         $data = $this->getMenuData();
         return $data;
