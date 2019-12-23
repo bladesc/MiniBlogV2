5df397496ebe850dfee404c30f7f6f1ca40dd478
Damian Szczesny <damian.szczesny@jinnilotto.com>
2019-12-23 19:34:10 +0100

    added breadcrump

--- src/template/default/layout/breadcrump.php
+++ src/template/default/layout/breadcrump.php
@@ -1,3 +1,23 @@
+<div class="box-page-body">
 <div id="box-breadcrump">
-    breadcrump
+    <?php
+    $page = '';
+    if ($this->request->query()->has('page')) {
+        $page = $this->request->query()->get('page');
+    } elseif ($this->request->query()->has('cmspage')) {
+        $page = $this->request->query()->get('cmspage');
+    }
+    ?>
+
+    <div>
+        <ul>
+            <li><a  href="index.php">Home</a></li>
+            <?php if(!empty($page)): ?>
+                <li><span><?= $page ?></span></li>
+            <?php endif; ?>
+            <div class="box-clear-foot"></div>
+        </ul>
+    </div>
+
+</div>
 </div>
